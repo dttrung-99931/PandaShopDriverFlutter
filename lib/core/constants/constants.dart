c737daaa8e2f6151ad2cc4aa34d6356288e62eda
ptt <ptt99931.work@gmail.com>
2024-10-31 23:53:58 +0700

    [delivery] handle post delivery tracking while moving

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class Constants {
   static const sucessfulStatusCodes = [201, 200];
   static const apiSecondsTimeout = Duration(seconds: 30);
+  static const delvieryTrackingSecInterval = 3;
 }
 
 const emptyWidget = SizedBox.shrink();
